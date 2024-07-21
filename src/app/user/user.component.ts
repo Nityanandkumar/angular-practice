7849e3d09b34f9df488efc7077d986a30017214b
nityab1 <nityanandsunny@gmail.com>
2024-07-21 19:47:27 +0530

    used ouput decorator, output function to emit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,EventEmitter,Input, input ,Output} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
@@ -10,13 +10,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required:true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required:true}) name!: string;
+   //avatar = input.required<string>(); input function produces Inputsignal
+   @Output() select = new EventEmitter<string>();
+   // output decorator, Event emitter object
+   //select = Output<string>(); output function
 
      get imagePath() {
       return 'assets/users/'+ this.avatar;
      }
 
    onSelectUser() {
+    this.select.emit(this.id);
    }
 }

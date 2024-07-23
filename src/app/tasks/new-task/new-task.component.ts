4fb3f960b8ebde68622d11fbdf7bb707b0b8299e
nityab1 <nityanandsunny@gmail.com>
2024-07-23 10:24:03 +0530

    implemented ngModel directive, to get input value and write it back over that input

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,14 +1,20 @@
 import { Component, EventEmitter,Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+/*for the sake of registering ngModule we importing FormsModule. 
+Forms Module is an collection of directives and features that help ypu with dealing the forms and with user input */
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel=new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
 onCancel() {
 this.cancel.emit();

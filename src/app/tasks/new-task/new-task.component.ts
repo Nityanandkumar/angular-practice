155546ce2d60d7ca56730d454335c4127d3d9130
nityab1 <nityanandsunny@gmail.com>
2024-07-23 11:14:50 +0530

    added onsubmit method and able to create the task in tasks section

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter,Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { newTaskData } from '../task/task.model';
 /*for the sake of registering ngModule we importing FormsModule. 
 Forms Module is an collection of directives and features that help ypu with dealing the forms and with user input */
 
@@ -12,11 +13,19 @@ Forms Module is an collection of directives and features that help ypu with deal
 })
 export class NewTaskComponent {
   @Output() cancel=new EventEmitter<void>();
+  @Output() add = new EventEmitter<newTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
 onCancel() {
 this.cancel.emit();
+}
+onSubmit() {
+   this.add.emit({
+     title : this.enteredTitle,
+     summary: this.enteredSummary,
+     date: this.enteredDate,
+   })
 }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
+import { type newTaskData } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
@@ -10,8 +11,8 @@ import { NewTaskComponent } from "./new-task/new-task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input ({required:true}) userId?: string;
-  @Input({required:true}) name?: string;
+  @Input ({required:true}) userId!: string;
+  @Input({required:true}) name!: string;
   isAddTaskEnabled = false;
   tasks= [
      {
@@ -50,5 +51,15 @@ export class TasksComponent {
   }
   onCancelAddTask() {
     this.isAddTaskEnabled=false;
+  }
+  onCreateAddTask(taskData : newTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date,
+    })
+    this.isAddTaskEnabled =false;
   }
  }

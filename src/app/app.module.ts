f07bdf2f1119fd903237e32caf67731e5d889d85
nityab1 <nityanandsunny@gmail.com>
2024-07-24 22:49:20 +0530

    bootstrapping Apps with angular module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import {NgModule} from '@angular/core';
-
-@NgModule ()
+import { AppComponent } from './app.component';
+//ngModule is an module . we are using is replacement of standalone components and standalone components are recommended way.
+// modules are grouping components together and structing an application
+@NgModule ({
+    declarations : [AppComponent],// we need to initalize all the components here. like grouping
+    bootstrap: [AppComponent] //it typicallys just one, that consists of single tree of components with which i wanna start application
+})
 
 export class AppModule{}

5a2a35e6873b2a7a29a064d61c5589be562eb839
nityab1 <nityanandsunny@gmail.com>
2024-07-24 23:35:35 +0530

    created and using shared module in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { SharedModule } from './shared/shared.module';
 
 //ngModule is an module . we are using is replacement of standalone components and standalone components are recommended way.
 // modules are grouping components together and structing an application
-@NgModule ({
-    declarations : [AppComponent,HeaderComponent,UserComponent,CardComponent,TasksComponent,TaskComponent,NewTaskComponent],// we need to initalize all the components here. like grouping. for non standalone components
-    bootstrap: [AppComponent], //it typicallys just one, that consists of single tree of components with which i wanna start application
-    imports:[BrowserModule,FormsModule] // imports array is an not just  for enabling  standalone components but also for including other modules
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ], // we need to initalize all the components here. like grouping. for non standalone components
+  bootstrap: [AppComponent], //it typicallys just one, that consists of single tree of components with which i wanna start application
+  imports: [BrowserModule, FormsModule, SharedModule], // imports array is an not just  for enabling  standalone components but also for including other modules
 })
-
-export class AppModule{}
+export class AppModule {}

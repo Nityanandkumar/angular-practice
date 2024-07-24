489dbf0acb6991656b60ebef742ba6c9cad5c861
nityab1 <nityanandsunny@gmail.com>
2024-07-24 23:48:38 +0530

    created more complex modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskComponent } from './tasks/task/task.component';
-import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
 
 //ngModule is an module . we are using is replacement of standalone components and standalone components are recommended way.
 // modules are grouping components together and structing an application
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent,
-    TaskComponent,
-    NewTaskComponent,
-  ], // we need to initalize all the components here. like grouping. for non standalone components
+  declarations: [AppComponent, HeaderComponent, UserComponent], // we need to initalize all the components here. like grouping. for non standalone components
   bootstrap: [AppComponent], //it typicallys just one, that consists of single tree of components with which i wanna start application
-  imports: [BrowserModule, FormsModule, SharedModule], // imports array is an not just  for enabling  standalone components but also for including other modules
+  imports: [BrowserModule,SharedModule,TasksModule], // imports array is an not just  for enabling  standalone components but also for including other modules
 })
 export class AppModule {}
